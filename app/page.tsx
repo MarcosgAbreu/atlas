46205e3ab59906ec0fa1f1295ca3b07234b0076f
unknown <abreumarcos13462@gmail.com>
2024-04-29 11:41:40 -0300

    continuando

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
-import Image from "next/image";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  let loggedIn = false;
+
+  try {
+    const supabase = createServerComponentClient({cookies})
+    const { 
+      data: { session },
+    } = await supabase.auth.getSession
+
+    if (session) redirect("/user-app")
+
+  } catch (error) {
+    console.log("Home" + error)
+  }
+
   return (
     <div className="text-lime-700">home</div>
   );
